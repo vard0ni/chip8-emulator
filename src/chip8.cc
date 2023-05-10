696359d6b26eeb7f6e6672af2406608734ed4614
vard0ni <zahargapeev@gmail.com>
2023-05-10 19:35:14 +0700

    Rename some base variables and add loadROM function

--- src/chip8.cc
+++ src/chip8.cc
@@ -1,8 +1,11 @@
+#include <cstdio>
 #include <string.h>
+#include <fstream>
 #include "chip8.h"
 
 // font set of 16 characters at 5 bytes each for rendering
 // 16 * 5 = 80 bytes size fontset
+// Each number or character is 4 pixels wide and 5 pixel high.
 const unsigned char fontset[FONTSET_SIZE] =
 {
     0xF0, 0x90, 0x90, 0x90, 0xF0, //0
@@ -25,58 +28,86 @@ const unsigned char fontset[FONTSET_SIZE] =
 
 chip8::chip8()
 {
+    // Initialize PC
+	pc = START_ADDRESS;
 
+    // Load fonts into memory
+	for (unsigned int i = 0; i < FONTSET_SIZE; ++i)
+	{
+		memory[FONTSET_START_ADDRESS + i] = fontset[i];
+	}
 }
 
-chip8::~chip8()
+// function that loads the contents of a ROM file
+void chip8::LoadROM(char const* filename)
 {
+	// Open the file as a stream of binary and move the file pointer to the end
+	std::ifstream file(filename, std::ios::binary | std::ios::ate);
 
-}
+	if (file.is_open())
+	{
+		// Get size of file and allocate a buffer to hold the contents
+		std::streampos size = file.tellg();
+		char* buffer = new char[size];
 
-void chip8::init()
-{
-    pctr    = 0x200;    // program counter starts at 0x200
-    opcode  = 0;        // reset current opcode
-    I       = 0;        // reset index register
-    sptr    = 0;        // reset stack pointer
+		// Go back to the beginning of the file and fill the buffer
+		file.seekg(0, std::ios::beg);
+		file.read(buffer, size);
+		file.close();
 
-    int i;
+		// Load the ROM contents into the Chip8's memory, starting at 0x200
+		for (long i = 0; i < size; ++i)
+		{
+			memory[START_ADDRESS + i] = buffer[i];
+		}
 
-    // clear display
-    for (i = 0; i < DISPLAY_SIZE; ++i)
-        display[i] = 0;
-
-    // clear stack
-    for (i = 0; i < STACK_SIZE; ++i)
-        stack[i] = 0;
-
-    // clear registers
-    for (i = 0; i < NUM_REGISTERS; ++i)
-        V[i] = 0;
-
-    // clear keypad
-    for (i = 0; i < NUM_KEYS; ++i)
-        key[i] = 0;
-
-    // clear memory
-    for (i = 0; i < SYSTEM_MEMORY; ++i)
-        memory[i] = 0;
-
-    // load fontset
-    for (i = 0; i < FONTSET_SIZE; ++i)
-        memory[i] = fontset[i];
-
-    // reset timers
-    delay_timer = 0;
-    sound_timer = 0;
+		// Free the buffer
+		delete[] buffer;
+	}
 }
 
 void chip8::emulateCycle()
 {
     // fetch opcode
+    opcode = memory[pc] << 8 | memory[pc + 1];
 
-    // execute opcode
+    // decode opcodes
+    switch (opcode & 0xF000)
+    {
+        case 0x0000:
+            switch (opcode & 0X00F)
+            {
+                case 0x0000:    // 0x00E0: Clears the screen
+                    // execute opcode
+                break;
+            
+                case 0X000E:    // 0x00EE: Returns from subroutine 
+                    // execute opcode
+                break;
+        
+                default:
+                    printf("Unknown opcode [0x0000]: 0x%X\n", opcode);
+            }                
+        break;
 
-    //
+        // more opcodes
+
+        case 0X2000:    // 0x2NNN: Calls subroutine at NNN.
+            stack[sp] = pc;        // Store current address in stack
+            ++sp;                    // Increment stack pointer
+            pc = opcode & 0x0FFF;    // Set the program counter to the address at NNN
+        break;
+    }
+
+    // update timers
+    if (delay_timer > 0)    
+        --delay_timer;
+
+    if (sound_timer > 0)
+    {
+        if (sound_timer == 1)
+            printf("BOOM\n");
+        --sound_timer;
+    }
 }
 
--- src/chip8.h
+++ src/chip8.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 #define SYSTEM_MEMORY   4096
 #define DISPLAY_SIZE    2048
 #define FONTSET_SIZE    80
@@ -7,32 +9,33 @@
 #define NUM_REGISTERS   16
 #define NUM_KEYS        16
 
+const unsigned int FONTSET_START_ADDRESS = 0x50;
+const unsigned int START_ADDRESS = 0x200;
+
 class chip8
 {
 private:
-    unsigned short opcode;                // current opcode
-    unsigned short I;                     // index register for draw
-    unsigned short pctr;                  // program counter
-    unsigned short sptr;                  // stack pointer
+    uint16_t opcode;                    // current opcode
+    uint16_t index;                     // index register for draw
+    uint16_t pc;                        // program counter
+    uint8_t sp;                         // stack pointer
 
-    unsigned char memory[SYSTEM_MEMORY];  // Memory(size = 4k)
-    unsigned char V[NUM_REGISTERS];       // V-registers(V0..VF)    
-    unsigned short stack[STACK_SIZE];     // Stack(16 16-bit values)
+    uint8_t memory[SYSTEM_MEMORY];      // Memory(size = 4k)
+    uint8_t registers[NUM_REGISTERS];   // V-registers(V0..VF)    
+    uint16_t stack[STACK_SIZE];         // Stack(16 16-bit values)
 
-    
 
-    unsigned char delay_timer;
-    unsigned char sound_timer;
-
-    void init();
+    uint8_t delay_timer;
+    uint8_t sound_timer;
 
 public:
     chip8();
-    ~chip8();
 
-    unsigned char display[DISPLAY_SIZE];
-    unsigned char key[NUM_KEYS];          // hex based keypad(0x0-0xF)
-
+    void LoadROM(char const* filename);
     void emulateCycle();
 
+    uint32_t display[DISPLAY_SIZE];
+    uint8_t key[NUM_KEYS];              // hex based keypad(0x0-0xF)
+
+    
 };
